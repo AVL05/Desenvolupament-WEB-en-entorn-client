c1c753a2d5d49ca7f2be67dfc5e2439413cbb57f
Alex Vicente Lopez <99687243+AVL05@users.noreply.github.com>
2026-01-12 18:59:06 +0100

    Practica 3 de VUE

--- Tema 8/3. API Buscador Bebidas Ficheros/buscadorBebidas/src/App.vue
+++ Tema 8/3. API Buscador Bebidas Ficheros/buscadorBebidas/src/App.vue
@@ -1,23 +1,18 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+  import tailwindcss from '@tailwindcss/vite';
+    export default defineConfig({
+    plugins: [
+      vue(),
+      vueDevTools(),
+      tailwindcss()
+    ],
+  });
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  
+  <h1>Desde Pinia</h1>
 </template>
 
 <style scoped>
